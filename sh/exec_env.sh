44cb2693795bfeea8fb3efd432ac64699a61f2cf
lilahliza <130019744+lilahliza@users.noreply.github.com>
2023-04-06 12:02:09 +0800

    Update exec_env.sh

--- sh/exec_env.sh
+++ sh/exec_env.sh
@@ -3,3 +3,8 @@
 
 # php artisan make:migration add_index_visit_time_to_private_users_table  --env=wx792152cf7d4b3d45
 # php artisan make:migration add_index_soft_delete_to_shop_auths_table  --env=wx792152cf7d4b3d45
+
+php artisan command:test_env_server
+
+
+
