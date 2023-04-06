31b7c05d4bfb73e3b566a5a31e57f15451fabfd2
lilahliza <130019744+lilahliza@users.noreply.github.com>
2023-04-06 20:06:46 +0800

    1

--- bootstrap/environment.php
+++ bootstrap/environment.php
@@ -10,6 +10,68 @@
 | given environment, then we will automatically detect it for you.
 |
 */
+
+
+
+$envPrefix = '';
+
+// form artisan
+if (app()->runningInConsole()) {
+    // 运行在命令行下
+    foreach ($_SERVER['argv'] as $strArg) {
+        if (preg_match("/--env=(.+)/", $strArg, $match) && !empty($match[1])) {
+            $envPrefix = $match[1];
+            break;
+        }
+    }
+} else {
+    // form server
+    if (key_exists('SUBDOMAIN', $_SERVER)) {
+        $envPrefix = $_SERVER['SUBDOMAIN'];
+    }
+}
+
+//判断是否在白名单内
+if (empty($envPrefix) || empty(env($envPrefix))) {
+    //运行在命令行
+    if (app()->runningInConsole()) {
+        die('[error] no environment');
+    } else {
+        $e = '{
+            "code": "0",
+            "msg": "请求不在白名单内"
+        }';
+        die($e);
+    }
+}
+
+
+$envForme = '.' . $envPrefix . '.env';
+//判断文件是否存在
+$envPath = __DIR__ . '/../env';
+$envPathName = $envPath . '/' . $envForme;
+if (file_exists($envPathName)) {
+
+} else {
+    //运行在命令行
+    if (app()->runningInConsole()) {
+        die('[error] no environment');
+    } else {
+        $e = '{
+            "code": "0",
+            "msg": "请求不在白名单内"
+        }';
+        die($e);
+    }
+}
+
+//自定义env文件路径
+$app->useEnvironmentPath(realpath($envPath));
+
+# $app->loadEnvironmentFrom('.prod.env');
+$app->loadEnvironmentFrom($envForme);
+
+/**
 //加载完整个框架之前重载【指定项目的env】配置文件
 $app->afterLoadingEnvironment(function () use ($app) {
     $envPrefix = '';
@@ -80,3 +142,6 @@ $app->afterLoadingEnvironment(function () use ($app) {
     var_dump('==dotenv==',$dotenv);
     $dotenv->overload();
 });
+
+*/
+
