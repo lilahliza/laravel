897f81c768f8188e67881e79be19d67e5a2d56e6
lilahliza <130019744+lilahliza@users.noreply.github.com>
2023-04-07 14:08:21 +0800

    Update 2023_04_07_060323_create_activity_logs_table.php

--- database/migrations/2023_04_07_060323_create_activity_logs_table.php
+++ database/migrations/2023_04_07_060323_create_activity_logs_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('activity_logs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            
+            $table->tinyInteger('sale_level')->nullable($value = false)->default(0)->comment('标志');
+            
+            $table->comment = '存活日志表';
+            
         });
     }
 
