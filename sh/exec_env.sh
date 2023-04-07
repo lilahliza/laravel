c11e07b23fd6e3034c47dd77eca68b9817d1df8b
lilahliza <130019744+lilahliza@users.noreply.github.com>
2023-04-07 14:16:15 +0800

    Update exec_env.sh

--- sh/exec_env.sh
+++ sh/exec_env.sh
@@ -3,10 +3,11 @@
 
 
 
-php artisan command:test_env_server  --env=b43d45
-php artisan command:test_env_server  --env=4b3d45
+# php artisan command:test_env_server  --env=b43d45
+# php artisan command:test_env_server  --env=4b3d45
 
-php artisan command:test_env_server  --env=duwbai
-php artisan make:model ActivityLog -m  --env=duwbai
+
+# php artisan make:model ActivityLog -m  --env=duwbai
 php artisan migrate  --env=duwbai
+php artisan command:test_env_server  --env=duwbai
 # php artisan migrate:rollback  --env=duwbai
