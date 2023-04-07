3c1ef934151a6fe626d3c35f4fad03f5f65c7ce5
lilahliza <130019744+lilahliza@users.noreply.github.com>
2023-04-07 10:39:27 +0800

    Update exec_env.sh

--- sh/exec_env.sh
+++ sh/exec_env.sh
@@ -7,5 +7,5 @@ php artisan command:test_env_server  --env=b43d45
 php artisan command:test_env_server  --env=4b3d45
 
 php artisan command:test_env_server  --env=duwbai
-
+php artisan migrate  --env=duwbai
 
